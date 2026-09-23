ecd43dcb40612c5d1542a970dfddfd357d135766
ezportas-web <ezportas@gmail.com>
2026-09-23 14:29:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * TROCOU O index.html? Mude o número da versão abaixo; ele também limpa o
  * que ficou de trás.
  */
-const VERSAO = 'despacho-v11';
+const VERSAO = 'despacho-v13';
 
 const CASCA = [
   './',
